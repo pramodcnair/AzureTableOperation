de19776d939ef820bd69c286fff69d54d3c67acf
Pramod Nair <mailtopramod@gmail.com>
2020-01-30 12:06:28 +0530

    Contact Tables

---
+++ Models/Concrete/TContactRepository.cs
@@ -0,0 +1,94 @@
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using ContactsCoreMVC.Models.Abstract;
+using ContactsCoreMVC.Models.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ContactsCoreMVC.Models.Concrete
+{
+    public class TContactRepository : IContactRepository
+    {
+        private readonly CloudStorageAccount _cloudStorageAccount;
+        private readonly CloudTableClient _cloudTableClient;
+        private readonly CloudTable _cloudTable;
+
+        public TContactRepository(IOptions<StorageUtility> storageUtility)
+        {
+            _cloudStorageAccount = storageUtility.Value.StorageAccount;
+            _cloudTableClient = _cloudStorageAccount.CreateCloudTableClient();
+            _cloudTable = _cloudTableClient.GetTableReference("Contacts");
+            _cloudTable.CreateIfNotExistsAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task<ContactTable> CreateAsync(ContactTable contactTable)
+        {
+            TableOperation insertOperation = TableOperation.Insert(contactTable);
+            TableResult tableResult = await _cloudTable.ExecuteAsync(insertOperation);
+            ContactTable insertedContact = tableResult.Result as ContactTable;
+            return insertedContact;
+        }
+
+        public async Task<List<ContactTable>> GetAllContactsAsync()
+        {
+            TableQuery<ContactTable> query = new TableQuery<ContactTable>();
+            TableContinuationToken tcToken = null;
+            var result = await _cloudTable.ExecuteQuerySegmentedAsync(query, tcToken);
+            return result.Results;
+        }
+
+        public async Task<ContactTable> FindContactAsync(string partitionKey, string rowKey)
+        {
+            TableOperation retrieveOperation = TableOperation.Retrieve<ContactTable>(partitionKey, rowKey);
+            TableResult tableResult = await _cloudTable.ExecuteAsync(retrieveOperation);
+            var result = tableResult.Result as ContactTable;
+            return result;
+        }
+
+        public async Task<List<ContactTable>> FindContactByRowKeyAsync(string rowKey)
+        {
+            TableQuery<ContactTable> query = new TableQuery<ContactTable>().Where(TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, rowKey));
+            TableContinuationToken tcToken = null;
+            var contactTableResult = await _cloudTable.ExecuteQuerySegmentedAsync(query, tcToken);
+            return contactTableResult.Results;
+        }
+
+        public async Task<List<ContactTable>> FindContactsByPartitionKeyAsync(string partitionKey)
+        {
+            TableQuery<ContactTable> query = new TableQuery<ContactTable>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, partitionKey));
+            TableContinuationToken tcToken = null;
+            var contactTableResult = await _cloudTable.ExecuteQuerySegmentedAsync(query, tcToken);
+            return contactTableResult.Results;
+        }
+
+        public async Task<ContactTable> UpdateAsync(ContactTable contactTable)
+        {
+            TableOperation retrieveOperation = TableOperation.Retrieve<ContactTable>(contactTable.PartitionKey, contactTable.RowKey);
+            TableResult tableResult = await _cloudTable.ExecuteAsync(retrieveOperation);
+            var contactToUpdate = tableResult.Result as ContactTable;
+            if (contactToUpdate != null)
+            {
+                contactToUpdate.ContactType = contactTable.ContactType;
+                contactToUpdate.Email = contactTable.Email;
+                TableOperation updateContact = TableOperation.Replace(contactToUpdate);
+                var updateResult = await _cloudTable.ExecuteAsync(updateContact);
+                return updateResult.Result as ContactTable;
+            }
+            return null;
+        }
+
+        public async Task DeleteAsync(string partitionKey, string rowKey)
+        {
+            TableOperation retrieveOperation = TableOperation.Retrieve<ContactTable>(partitionKey, rowKey);
+            TableResult tableResult = await _cloudTable.ExecuteAsync(retrieveOperation);
+            var contactToDelete = tableResult.Result as ContactTable;
+            if (contactToDelete != null)
+            {
+                TableOperation deleteContact = TableOperation.Delete(contactToDelete);
+                await _cloudTable.ExecuteAsync(deleteContact);
+            }
+
+        }
+    }
+}
---
+++ Models/Entities/ContactTable.cs
@@ -0,0 +1,17 @@
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ContactsCoreMVC.Models.Entities
+{
+    public class ContactTable : TableEntity
+    {
+        public ContactTable() { }
+        public ContactTable(string contactName, string phone)
+        {
+            PartitionKey = contactName;
+            RowKey = phone;
+        }
+        public string ContactType { get; set; }
+        public string Email { get; set; }
+    }
+
+}
